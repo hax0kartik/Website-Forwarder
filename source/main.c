ccc9c9d91a2222851847fa670b6840ef948f8c85
Pirater12 <pokemonemerald0@gmail.com>
2017-04-13 21:43:05 +0530

    Initial Commit

---
+++ source/main.c
@@ -0,0 +1,16 @@
+#include <3ds.h>
+/*
+IMPORTANT :- Do not remove any function , or the program will not work.
+Replace https://reddit.com/r/3dshacks with the site you want to make the forwader for
+Replace 29 with the len of the provided link(strlen).
+*/
+int main()
+{
+	gfxInitDefault();
+	APT_PrepareToStartSystemApplet(0x114);
+	APT_StartSystemApplet(0x114, "https://reddit.com/r/3dshacks",29, 0);
+	gspWaitForVBlank0();
+	gfxSwapBuffers();
+	gfxExit();
+	return 0;
+}
